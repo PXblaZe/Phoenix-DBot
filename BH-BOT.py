250ed718df49fa5b93944158396c9f15cea6e6fa
MrSumit <35930711+MrSumit@users.noreply.github.com>
2020-11-03 02:53:07 +0530

    Add files via upload
    1. Added on_member_join event.
    2. Added -accept command.
    3. Added some more functionality to -select command and now it have one -link aliases.
    4. Changed the name of link function to liinks

--- BH-BOT.py
+++ BH-BOT.py
@@ -1,3 +1,5 @@
+#modify select command
+
 import os
 import coc
 import asyncio
@@ -12,13 +14,15 @@ bot = commands.Bot(command_prefix = '-', intents = discord.Intents.all())
 g = Github(os.environ['git_token-BH'])
 repo = g.get_user().get_repo('DiscordBot')
 
-def append(file = 'Data.bh', data = '', index = -1):
+
+
+def append(data: str, file = 'Data.bh', index = -1):
     cnts = repo.get_contents(file)
     lines = cnts.decoded_content.decode().split('\n')
     lines.insert(index, data)
     repo.update_file(cnts.path, f'appended new member data at index {index}', '\n'.join(lines), cnts.sha)
 
-def link(server_id = os.environ['BH-serv_id'], file = 'Data.bh', t2m = True):
+def links(server_id = os.environ['BH-serv_id'], file = 'Data.bh', t2m = True):
     mapin = dict()
     server_id = int(server_id)
     cnts = repo.get_contents(file)
@@ -31,14 +35,32 @@ def link(server_id = os.environ['BH-serv_id'], file = 'Data.bh', t2m = True):
     return mapin
 
 
-
-''' EVENTS '''
+'''Discord EVENTS '''
 
 @bot.event
 async def on_ready():
     print('Bot is ready...')
 
-    
+@bot.event
+async def on_member_join(new : discord.Member):
+
+    wlcmsg = f'''Hey {new.mention}, Welcome to  :broken_heart: Broken Hearts\** :broken_heart: !
+    **
+    We only accept <:th10:769275244262588437>TH10, <:th11:769275245152043048>TH11, <:th12:769291986221924412>TH12 and <:th13:769292071692795966>TH13 !
+
+    If you are interested to join our clan do the following:
+
+      1.** Send a screenshot of your in game profile, with your player tag visible.
+    **2.** Send your player tag in text form. 
+    **3.** Send a screenshot of your base. It must be in a war base slot.
+    **4.** Wait for an answer from one of the staff.
+
+   *If you need any help ask it in {new.guild.get_channel(765501711694954518).mention} channel.* :slight_smile:'''
+    role = discord.utils.get(new.guild.roles, name = '[new]')
+    await new.add_roles(role)
+    await new.guild.get_channel(765497400398708737).send(wlcmsg)
+
+
 ''' COC EVENTS '''
 
 async def cocev():
@@ -46,7 +68,7 @@ async def cocev():
     @client.event
     @coc.ClanEvents.member_role(tags=["#229Y8VYP2"])
     async def on_role_updates(old_player, new_player):
-        timap = link()
+        timap = links()
         if str(new_player.tag) in timap:
             if str(old_player.role) == 'Co-Leader':
                 r2 = discord.utils.get(timap[new_player.tag].guild.roles, name = f'[{old_player.role[:2]}]')
@@ -65,7 +87,7 @@ async def cocev():
     @client.event
     @coc.ClanEvents.member_join(tags=["#229Y8VYP2"])
     async def foo(player, clan):
-        timap = link()
+        timap = links()
         if str(player.tag) in timap:
             r1 = discord.utils.get(timap[player.tag].guild.roles, name = '[Member]')
             r2 = discord.utils.get(timap[player.tag].guild.roles, name = '[WaitingList]')
@@ -76,31 +98,55 @@ async def cocev():
     @client.event
     @coc.ClanEvents.member_name(tags=["#229Y8VYP2"])
     async def foo(old_name, new_name, player):
-        ti = link()
+        ti = links()
         if str(player.tag) in ti:
             cr = ['[Leader]', '[Co]', '[Elder]', '[Member]']
             for dr in ti[player.tag].roles:
                 if dr.name in cr:
                     await ti[player.tag].edit(nick = f'{dr.name} {new_name}')
                     break
-
      
 
 ''' COMMANDS '''
 
-@commands.has_any_role('[Admin]', '[Leader]', '[Co]')
-@bot.command()
+@commands.has_any_role('[Admin]', '[Leader]', '[Co]', '[Elder]')
+@bot.command(aliases = ['link'])
 async def select(ctx, discord_member: discord.Member, player_tag):
-    role = discord.utils.get(discord_member.guild.roles, name="[WaitingList]")
-    r2 = discord.utils.get(discord_member.guild.roles, name="[new]")
     player = await client.get_player(player_tag)
-    th = player.town_hall
-    await discord_member.edit(nick = f'[TH{th}] {player.name}')
-    append(data = f'{discord_member.id} {player_tag}')
-    await discord_member.add_roles(role)
-    await discord_member.remove_roles(r2)
-    await ctx.send('**Successfully Selected.**')
+    for role in discord_member.roles:
+        if role.name == '[new]':
+            role = discord.utils.get(discord_member.guild.roles, name="[WaitingList]")
+            r2 = discord.utils.get(discord_member.guild.roles, name="[new]")
+            th = player.town_hall
+            await discord_member.edit(nick = f'[TH{th}] {player.name}')
+            append(data = f'{discord_member.id} {player_tag}')
+            await discord_member.add_roles(role)
+            await discord_member.remove_roles(r2)
+            selm = f'''
+Hey {discord_member.mention}, **Your base is selected !!!**
 
+**You are now in <@&765500587269292032>, if we don't have spot in our clan.
+
+We will dm you a shortlist message if you will be accepted by,
+
+<@&769125392916676608>, <@&769116913234083850> or <@&765480944416981023> of our clan.
+
+Till than checkout our channel make some new friends, we'll select you as
+
+soon as possible if any spot available in our clan.
+
+Feel free to use {discord_member.guild.get_channel(765501711694954518).mention} channel if you have any problem.**
+
+'''
+            await discord_member.guild.get_channel(765502125283082270).send(selm)
+            await ctx.send('**Successfully Selected.**')
+            break
+    else:
+        if discord_member.id in links(t2m=False): 
+            await ctx.send('***This discord member is already linked.***')
+        else:
+            append(data=f'{discord_member.id} {player_tag}')
+            await ctx.send('**Successfully Linked.**')
 @select.error
 async def foo(ctx, error):
     if isinstance(error, commands.errors.MissingRequiredArgument):
@@ -129,7 +175,7 @@ async def clan(ctx, clan_tag):
     val = ''
     for cm in cln.members:
         if str(cm.role) == 'Leader':
-            timap = link()
+            timap = links()
             if cm.tag in timap:
                 val = f'{timap[cm.tag].mention} (`{cm.name}`)'
             else:
@@ -167,7 +213,7 @@ async def foo(ctx, error):
 @bot.command(aliases = ['remove'])
 async def kick(ctx, member : discord.Member, reason = ''):
     guild = bot.get_guild(os.environ['BH-serv_id'])
-    if member.id in link(t2m=False):
+    if member.id in links(t2m=False):
         ct = repo.get_contents('Data.bh')
         lines = ct.decoded_content.decode().split('\n')
         for target in lines:
@@ -184,7 +230,23 @@ async def foo(ctx, error):
         await ctx.send(f'**Usage:** `{bot.command_prefix}kick Discord_Member reason`')
     if isinstance(error, Exception):
         await ctx.send(f'**Error:** {error}')
-    
+
+@commands.has_any_role('[Admin]', '[Leader]', '[Co]', '[Elder]')    
+@bot.command()
+async def accept(ctx, member: discord.Member):
+    for role in member.roles:
+        if role.name == '[WaitingList]':
+            accE = discord.Embed(
+                title = '💔 BROKEN HEARTS\** 💔',
+                description = f'\nHey {member.mention},\n\n**You are shortlisted to be a member of our Clan !!!**\n\nYou will have invite send from our clan if not\njust ask it with the screenshot of this message\nin {member.guild.get_channel(765501711694954518).mention} channel of our discord server.',
+                url = 'https://discord.gg/DZYbQHs7cd'        
+            )
+            await member.send(embed=accE)
+            await ctx.send('**Successfully Accepted.**')
+            break
+    else:
+        await ctx.send(f'**Error:** member {member.mention} is not in [WaitingList]')
+
 
 @bot.command()
 async def ping(ctx):
@@ -193,4 +255,3 @@ async def ping(ctx):
 
 bot.loop.create_task(cocev())
 bot.run(os.environ['BH-BOT_Token'])
-
